08f7a793ccabc24ee20213216be12482822459e6
alumno <0323aruiz@e-itaca.es>
2021-09-30 16:05:21 +0100

    Ejercicios 13 funciones de ficheros distintos

--- PrimerosEjercicios/Ejercicio13.php
+++ PrimerosEjercicios/Ejercicio13.php
@@ -8,6 +8,13 @@
 <body>
 <?php
 
+$num=1;
+$num1=3;
+$num2=1;
+//require __DIR__ . '/mates.php';
+require_once('mates.php');
+ 
+echo "Los resultados son :" , print_r(SegundoGrado($num,$num1,$num2));
 
 
 ?>
